219da542bbf54ab5c8b455cd684cb4970a045cce
Inga Titchiev <inga.titchiev@gmail.com>
2020-08-12 22:17:37 +0300

    Exemple lectia 5

---
+++ LoveProgramming.java
@@ -0,0 +1,8 @@
+public class LoveProgramming{
+	public static void main(String[] args)
+	{
+	System.out.println("I love Java programming");
+
+	}
+
+}
---
+++ NameSurname.java
@@ -0,0 +1,8 @@
+public class NameSurname{
+	public static void main(String[] args)
+	{
+	System.out.println("My name is Inga Titchiev");
+
+	}
+
+}
