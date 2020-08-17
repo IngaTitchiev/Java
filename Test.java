8e76ba505a4a237a25e346f412b048bd3c244725
Inga Titchiev <inga.titchiev@gmail.com>
2020-08-17 18:03:33 +0300

    exemplu lectia 6

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] params) {
+        PrimaClasa ref=new PrimaClasa();
+        byte b=60;
+        byte c=120;
+        int d;
+        d=b*c;
+        String str1="Un sir", str2="Alt sir";
+        System.out.println(d);
+        System.out.println(str1+str2);
+    }
+}
