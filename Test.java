778bc76f1561925b8441f383dadd2cab7f0951f3
Inga Titchiev <inga.titchiev@gmail.com>
2020-08-20 18:12:31 +0300

    Lectia 7

--- Test.java
+++ Test.java
@@ -8,5 +8,25 @@ public class Test {
         String str1="Un sir", str2="Alt sir";
         System.out.println(d);
         System.out.println(str1+str2);
-    }
+        int count1 = 1;
+        while (count1 <= 2) {
+            int count2 = 1;
+            while (count2 <= 3) {
+                System.out.println("Here");
+                count2++;
+            }
+            count1++;
+        }
+int k=1;
+        byte delta=1;
+        while(k>0&&k<100){
+            if(k>=99){
+                delta=-1;
+            }else if(k<=1){delta=1;
+            }
+            k+=delta;
+            System.out.println(k);
+        }
+
+        }
 }
