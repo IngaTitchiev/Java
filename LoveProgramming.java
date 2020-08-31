94d66f1423f495bb14e9c1f881c03ac3d17e6556
Inga Titchiev <inga.titchiev@gmail.com>
2020-08-31 13:14:39 +0300

    Curatare git

--- LoveProgramming.java
+++
@@ -1,8 +0,0 @@
-public class LoveProgramming{
-	public static void main(String[] args)
-	{
-	System.out.println("I love Java programming");
-
-	}
-
-}
--- NameSurname.java
+++
@@ -1,8 +0,0 @@
-public class NameSurname{
-	public static void main(String[] args)
-	{
-	System.out.println("My name is Inga Titchiev");
-
-	}
-
-}
